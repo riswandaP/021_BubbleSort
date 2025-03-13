ad1fe79a782091c118c8deea5905232b5246eb06
riswandaP <riswandaputra03@gmail.com>
2025-03-13 11:48:23 +0700

    Second Commit

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,3 +2,18 @@
 #include <string>
 using namespace std;
 
+int arr[20];                // Deklarasi Variabel global array a dengan ukuran 20
+int n;                      // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+
+void input() {              // Prosedur untuk input
+    while(true) {
+        cout << "Masukkan banyaknya elemen pada array : ";  // Output ke layar
+        cin >> n;           // Input dari pengguna
+        if (n <= 20)        // Jika data
+            break;          // Menghentikan loop
+        else{
+            cout << "\nArray dapat mempunyai maksimal 20 elemen\n" << endl;
+        }
+    }
+
+
